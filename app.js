590b13089a7388aee5e83f52d8397279be49fe7f
usman-naeem728 <76055167+usman-naeem728@users.noreply.github.com>
2021-01-05 18:06:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,247 @@
+
+
+Skip to content
+Using Gmail with screen readers
+Meet
+New meeting
+Join a meeting
+Hangouts
+
+1 of 439
+(no subject)
+Inbox
+
+Muhammad Hassan Naeem
+Attachments
+5:46 PM (2 minutes ago)
+to me
+
+
+Attachments area
+
+// chapter # 1
+
+// alert("Error!Please enter a valid password")
+
+// alert("Welcome to JS land.. \n Happy coding")
+
+// console.log("Hello..I can run JS through web browser's console ")
+
+
+// Chapter # 2
+
+// var userName
+
+// var myName ="Muhammad usman naeem"
+
+// var message = "Hello world"
+// alert(message)
+
+// var studentsName = "Bilal noman"
+// var studentage = "15 years old"
+// var course = "Certified Mobile Application Development"
+// alert(studentsName)
+// alert(studentage)
+// alert(course)
+
+
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+// var email = "unitedpakistan22@gmail.com"
+// alert("MY email address is" + " " +email)
+
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book" + " " + book)
+
+// document.write("Yah! I can write HTML content through Javascript")
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬" )
+
+
+// Chapter # 3
+
+// var age = 20
+// alert("I am"+" " + age + " "+ "years old" )
+
+// var a = prompt("Enter Your name")
+// if(a === "usman"){
+//     alert("You have visited this site 14 times")
+// }
+
+// var birthYear = 2001
+// document.write("MY birth year is"+ " "+ birthYear)
+
+// var visitorsName = "John doe"
+// var productTitle = "T-shirts"
+// var quantity = 5
+// document.write(visitorsName+ " " + "ordered" + " " + quantity + " " + productTitle + " " + "on XYZ clothing store")
+
+// chapter # 4
+
+// var a = "one" , b = "two" , c = "three"
+
+// Illegal variables
+// var my name = "name"
+// var  5number = "numbers"
+// var @name = "XYZ"
+// var ,name = "xyz"
+
+// Legal variables
+
+// var name = "Xyz"
+// var myName = "xyz"
+// var $name = "xyz"
+// var my_name = "xyz"
+
+// document.write("Rules for naming JS variables"+"<br/>" )
+// document.write(" Variable names can only contain ______, ______, ______ and ______." + "For example $my_1stVariable" + "<br />")
+// document.write(" Variables must begin with a ______, ______ or _____. For example $name, _name or name d" + "<br />")
+// document.write(" Variable names are case _________ " + "<br/>")
+// document.write(" Variable names should not be JS _________ ")
+
+// chapter # 6-9
+
+// var a = 10
+// document.write("the value of a is" + " " + a + "<br />")
+// document.write("the value of ++a is" + " " + ++a + "<br />" + "Now the value of a is" + " " + a + "<br/>")
+// document.write("the value of a++ is" + " " + a++ + "<br />" + "Now the value of a is" + " " + a + "<br/>")
+// document.write("the value of --a is" + " " + --a + "<br />" + "Now the value of a is" + " " + a + " <br/>")
+// document.write("the value of -a-- is" + " " + a-- + "<br />" + "Now the value of a is" + " " + a + " <br/>")
+
+// var a = 2, b = 1
+// var c = --a - --b + ++b + b--
+// document.write(c)
+// document.write(--a + "<br/>")
+// document.write(--a - --b + "<br/>")
+// document.write(--a - --b + ++b + "<br/>")
+// document.write(--a - --b + ++b + b-- + "<br/>")
+
+// var name = prompt("Enter Your name")
+// if(name === "usman"){
+//     alert("Wellcome sir!")
+// }
+
+// var num = prompt("Enter Number", "0") 
+// var i = 0;
+// for (i = 1; i <= 10; i++) {
+//   document.write(  num + " x " + i + " = " + num * i + "<br/>");
+// }
+
+// var sub1 = prompt("Enter your 1 subject")
+// var sub2 = prompt("Enter your 2 subject")
+// var sub3 = prompt("Enter your 3 subject")
+// var a = sub1, b = sub2, c = sub3
+// var num = 100
+// var subNum1 = prompt("Enter obtained marks of subject 1")
+// var subNum2 = prompt("Enter obtained marks of subject 2")
+// var subNum3 = prompt("Enter obtained marks of subject 3")
+// var X = subNum1, Y = subNum2, Z = subNum3
+// document.write('<table border="1" cellspacing="0">');
+// if (a === "English" || b === "Urdu" || c === "Maths") {
+//     document.write("<tr><td>" + "SUBJECT" + "  " + "TotalMarks" + "<br/>" + a + " " + num + "<br/>" + b + " " + num + "<br/>" + c + " " + num + "<tr><td>")
+// }
+// for(i = 0 ; i < 4 ; i++){
+//    document.write(X + " " + Y + " " + Z)
+// }
+
+// chapter # 9-11
+
+// var name = prompt("Enter your city")
+// if(name === "Karachi"){
+//     alert("WEllcome to the city of lights")
+// }else{
+//     alert("Wellcome to the"+ " " + name)
+// }
+
+// var gender = prompt("Enter your gender")
+// if(gender === "Male"){
+//     alert("Good Morning Sir")
+// }else{
+//    alert ("Good Morning Ma'am")
+// }
+
+// var color = prompt("Enter traffic signal color")
+// if(color === "red"){
+//     alert("Must stop")
+// }if(color === "yellow"){
+//     alert("Ready to move")
+// }if(color === "green"){
+//     alert("Move now")
+// }
+
+// var a = prompt("Your car fuel in liters")
+// if(a <= 0.25){
+//     alert("Please refill the fuel")
+// }if(a > 0.25){
+//     alert( "enjoy your ride")
+// }
+
+// var a = 4; if (++a === 5){ alert("given condition for variable a is true"); }yes
+//  var b = 82; if (b++ === 83){ alert("given condition for variable b is true"); }no
+// var c = 12; if (c++ === 13){ alert("condition 1 is true"); }(no) if (c === 13){ alert("condition 2 is true"); }(yes) if (++c < 14){ alert("condition 3 is true"); }(yes) if(c === 14){ alert("condition 4 is true"); }(no) 
+ 
+// var materialCost = 20000; var laborCost = 2000; var totalCost = materialCost + laborCost; if (totalCost === laborCost + materialCost){ alert("The cost equals"); }(yes)
+
+//  if (true){ alert("True"); } if (false){ alert("False"); } 
+// if("car" < "cat"){ alert("car is smaller than cat"); } (yes)
+
+
+// var totalMarks1 = prompt("Enter total makrks of urdu")
+// var totalMarks2 = prompt("Enter total makrks of Maths")
+// var totalMarks3 = prompt("Enter total makrks of English")
+// var obtMarks1  = prompt("obtained marks of urdu")
+// var obtMarks2  = prompt("obtained marks of Maths")
+// var obtMarks3  = prompt("obtained marks of English")
+
+// var a = prompt("guess the number")
+// // var b = 5
+// if(a === "5"){
+//     alert("Bingo! Correct answer ")
+// }if(a === "6"){
+//      alert("close enough")
+//  }if(a === "4"){
+//      alert("close enough")
+//  }
+//  else{
+//      alert("wrong")
+// }
+
+// var num = prompt("enter a number divisible by 3")
+// var a = 3
+// if(num / 3 ){
+//     alert("correct")
+// }
+
+// var temp = prompt("temperatura in celsius")
+// if(temp > "40"){
+//     alert("It is too hot outside")
+// }
+// if(temp > "30"){
+//         alert("The Weather today is Normal")
+// }
+// if(temp > "20"){
+//     alert("“Today’s Weather is cool")
+// }
+// else{
+//     alert("OMG! Today’s weather is so Cool")
+// }
+
+// chapter # 12-13
+
+// var num1 = prompt("Input the First integer", "0");
+// var num2 = prompt("Input the second integer", "0");
+                                                 
+// if(parseInt(num1, 10) > parseInt(num2, 10)) 
+//   { 
+//   document.write("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");
+//   }   
+// else
+//   if(parseInt(num2, 10) > parseInt(num1, 10)) 
+//   {
+//   document.write("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");
+//   }                  
+// else
+//   {
+//    document.write("The values "+ num1+ " and "+num2+ " are equal.");
+//   }
+  
